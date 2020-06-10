fddadd7723b841f86d0fb57556cb2be864a0cb66
Giacomo Ferlaino <giacomo.ferlaino@outlook.com>
2020-06-10 15:38:31 +0200

    feat: Created base HTTP server

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	app := NewApp()
+
+	app.router.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		fmt.Fprint(res, "Hello world!")
+	})
+
+	http.ListenAndServe(":8080", app.router)
+}
