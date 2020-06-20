e0162b6f2959c6aae1de4ccdad29a42ea1760388
Giacomo Ferlaino <giacomo.ferlaino@outlook.com>
2020-06-20 17:56:47 +0200

    feat: Added POST method support for /connection endpoint

--- packages/httphandlers/connection.go
+++ packages/httphandlers/connection.go
@@ -1,24 +1,25 @@
 package httphandlers
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
-	"io"
+	"io/ioutil"
 	"net/http"
+
+	"github.com/giacomoferlaino/go-sql-microservice/packages/database"
 )
 
 // NewConnectionHandler allocates and returns a new ConnectionHandler.
 func NewConnectionHandler(appState AppState) *ConnectionHandler {
 	return &ConnectionHandler{
 		appState: appState,
-		write:    fmt.Fprint,
 	}
 }
 
 // ConnectionHandler maganges the avaiable database connections.
 type ConnectionHandler struct {
 	appState AppState
-	write    func(writer io.Writer, a ...interface{}) (length int, err error)
 }
 
 // ServeHTTP is the HTTP handler function for this handler.
@@ -26,6 +27,8 @@ func (h *ConnectionHandler) ServeHTTP(res http.ResponseWriter, req *http.Request
 	switch req.Method {
 	case "GET":
 		h.get(res, req)
+	case "POST":
+		h.post(res, req)
 	default:
 		pageNotFoundHandler(res, req)
 	}
@@ -34,8 +37,32 @@ func (h *ConnectionHandler) ServeHTTP(res http.ResponseWriter, req *http.Request
 func (h *ConnectionHandler) get(res http.ResponseWriter, req *http.Request) {
 	bsData, err := json.Marshal(h.appState.Databases())
 	if err != nil {
-		h.write(res, "error")
+		fmt.Fprintln(res, err)
 		return
 	}
-	h.write(res, string(bsData))
+	fmt.Fprintln(res, string(bsData))
+}
+
+func (h *ConnectionHandler) post(res http.ResponseWriter, req *http.Request) {
+	reqBody, err := ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	if err != nil {
+		res.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(res, err)
+		return
+	}
+	connectionOptions := &database.ConnectionOptions{}
+	err = json.Unmarshal(reqBody, connectionOptions)
+	if err != nil {
+		res.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(res, err)
+		return
+	}
+	dbConnection, err := sql.Open(connectionOptions.Driver, connectionOptions.ConnectionString())
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	h.appState.Databases().Store(connectionOptions.Name, dbConnection)
+	fmt.Fprintln(res, "Created connection: ", connectionOptions.Name)
 }
